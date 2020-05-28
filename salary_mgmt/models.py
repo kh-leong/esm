272afee94c5da4a2749961f311e2f59edbdf724b
Leong Keng Hoe <kenghoe112@yahoo.com.sg>
2020-05-28 21:41:22 +0800

    Changed login in DB to be unique

--- salary_mgmt/models.py
+++ salary_mgmt/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class EmployeeSalary(models.Model):
     id = models.CharField(primary_key=True, max_length=24)
-    login = models.CharField(max_length=24)
+    login = models.CharField(max_length=24, unique=True)
     name = models.CharField(max_length=100)
     salary = models.FloatField()
 
