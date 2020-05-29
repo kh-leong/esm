c57cc8888211f39ae35fa37585bd0a788a883d6a
Leong Keng Hoe <kenghoe112@yahoo.com.sg>
2020-05-29 16:40:31 +0800

    Misc comments

--- salary_mgmt/models.py
+++ salary_mgmt/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class EmployeeSalary(models.Model):
+    # assumption: id has maximum length of 24
     id = models.CharField(primary_key=True, max_length=24)
+    # assumption: login has maximum length of 24
     login = models.CharField(max_length=24, unique=True)
+    # assumption: name has maximum length of 100
     name = models.CharField(max_length=100)
     salary = models.FloatField()
 
